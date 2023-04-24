e219682c66692bb592e3ee3744361142526f29b9
Akshay Gulhane <argulhane10246@gmail.com>
2023-04-24 23:07:41 +0530

    Commit2404231

--- src/components/Pages/Functions/functionsdata.js
+++ src/components/Pages/Functions/functionsdata.js
@@ -1,3 +1,19 @@
 const functionsData = {
-    
+    topic : "Maps",
+    question : "what is map ",
+    answer : 
+   `1. A Map holds key-value pairs where the keys can be any datatype.
+2. A Map remembers the original insertion order of the keys.
+3. A Map has a property that represents the size of the map.`,
+
+    code :
+`const nums = [1,2,3,4];
+console.log("nums is => " + nums);
+const multiplyThree = nums.map((num,i,nums) => {
+return num * 3 ;
+// num is an individual number 
+// i is the index 
+// nums is the actual array we used
+})`,
+    explaination : "[3,6,9,12]"
 }
