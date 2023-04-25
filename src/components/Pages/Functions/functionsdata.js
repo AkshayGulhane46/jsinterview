9dbe31eff193e17f9efc0d7afb5939f44b3757e1
Akshay Gulhane <argulhane10246@gmail.com>
2023-04-25 23:46:41 +0530

    Commit2504231

--- src/components/Pages/Functions/functionsdata.js
+++ src/components/Pages/Functions/functionsdata.js
@@ -1,19 +1,58 @@
-const functionsData = {
-    topic : "Maps",
-    question : "what is map ",
+export const functionDeclaration = {
+    topic : "Function Definations",
+    question : "what is function declaration",
     answer : 
-   `1. A Map holds key-value pairs where the keys can be any datatype.
-2. A Map remembers the original insertion order of the keys.
-3. A Map has a property that represents the size of the map.`,
-
+   `Function Declaration is just the function with its implementation`,
     code :
-`const nums = [1,2,3,4];
-console.log("nums is => " + nums);
-const multiplyThree = nums.map((num,i,nums) => {
-return num * 3 ;
-// num is an individual number 
-// i is the index 
-// nums is the actual array we used
-})`,
-    explaination : "[3,6,9,12]"
+`function squaredeclare(number) {
+    return number * number
+}`,
+    explaination : ""
 }
+
+
+export const firstClassFunction = {
+    topic : "First Class Function",
+    question : "what is first class function",
+    answer : 
+   `1. we can pass functions as parameters of another function 
+   2. here the square will be passed as parameter of displaySquare()`,
+    code :
+`function displaySquare(fn){
+    console.log("Square is" + fn(5));
+}
+// here the square will be passed as parameter of displaySquare()
+displaySquare(square);
+`,
+    explaination : "25"
+}
+
+export const functionExpression = {
+    topic : "Function Expression",
+    question : "what is function Expression",
+
+    answer : 
+   `Function expression is the function which is assigned to a variable`,
+    code :
+`const square = function(num){
+    return num * num
+}`,
+    explaination : ""
+}
+
+export const iife = {
+    topic : "IIFE",
+    question : "what is IIFE",
+    answer : 
+   ``,
+    code :
+`const square = function(num){
+    return num * num
+}`,
+    explaination : ""
+}
+
+
+
+
+
